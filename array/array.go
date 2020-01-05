fc2a01e90f6675f6b2a690fa9fb3886b071882d3
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-01-05 16:00:33 +0700

    Add BubbleSort implementation in array

--- array/array.go
+++ array/array.go
@@ -1,5 +1,32 @@
 package array
 
+// BubbleSort sorts the array a using Bubble Sort algorithm
+func BubbleSort(a []int) (sorted []int) {
+	sorted = make([]int, len(a))
+	copy(sorted, a)
+
+	n := len(sorted)
+	for {
+		swapped := false
+
+		for i := 1; i < n; i++ {
+			if sorted[i-1] > sorted[i] {
+				temp := sorted[i-1]
+				sorted[i-1] = sorted[i]
+				sorted[i] = temp
+				swapped = true
+			}
+		}
+
+		if !swapped {
+			break
+		}
+		n--
+	}
+
+	return sorted
+}
+
 // Equal checks whether two slices a and b are identical (true) or not (false)
 // DEPRECATED, use cmp.Equal instead
 func Equal(a, b []int) bool {
--- array/array_test.go
+++ array/array_test.go
@@ -3,8 +3,18 @@ package array_test
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/ianadiwibowo/central-park/array"
 )
+
+func TestBubbleSort(t *testing.T) {
+	a := []int{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	expected := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+	result := array.BubbleSort(a)
+	if cmp.Equal(result, expected) != true {
+		t.Errorf("Test: %v. Expected: %v. Got: %v.", a, expected, result)
+	}
+}
 
 func TestEqual(t *testing.T) {
 	a := []int{1, 2, 3, 4, 5}
