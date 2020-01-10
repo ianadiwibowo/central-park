564e0408ab3f822b8cd63d3ebc2778203619d86d
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-01-11 02:23:26 +0700

    Add counting sort algorithm

--- array/array.go
+++ array/array.go
@@ -1,6 +1,8 @@
 package array
 
-import "sort"
+import (
+	"sort"
+)
 
 // BubbleSort sorts the array a using Bubble Sort algorithm
 func BubbleSort(a []int) (sorted []int) {
@@ -24,6 +26,30 @@ func BubbleSort(a []int) (sorted []int) {
 			break
 		}
 		n--
+	}
+
+	return sorted
+}
+
+// CountingSort sorts the array a using Counting Sort algorithm
+func CountingSort(a []int) []int {
+	if len(a) <= 1 {
+		return a
+	}
+
+	counter := make([]int, len(a))
+	for _, v := range a {
+		counter[v]++
+	}
+
+	for i := 1; i < len(counter); i++ {
+		counter[i] = counter[i] + counter[i-1]
+	}
+
+	sorted := make([]int, len(a))
+	for _, v := range a {
+		sorted[counter[v]-1] = v
+		counter[v]--
 	}
 
 	return sorted
