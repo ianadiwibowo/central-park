945d36d36cb9ad8a5cb0620aa73a2bfd32c13d3f
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-09-16 21:57:52 +0700

    Use Testify for Queue (interface{} version) unit tests

--- datastructures/queue/queue_test.go
+++ datastructures/queue/queue_test.go
@@ -3,8 +3,8 @@ package queue_test
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/ianadiwibowo/central-park/datastructures/queue"
+	"github.com/stretchr/testify/assert"
 )
 
 // cat demonstrates that a custom struct can be used as the queue internal data structure
@@ -16,9 +16,7 @@ type cat struct {
 func TestNewQueue(t *testing.T) {
 	q := queue.NewQueue()
 
-	if q.Print() != "[]" {
-		t.Errorf("Expected: []. Got: %v", q.Print())
-	}
+	assert.Equal(t, "[]", q.Print())
 }
 
 func TestEnqueue(t *testing.T) {
@@ -26,9 +24,7 @@ func TestEnqueue(t *testing.T) {
 	q.Enqueue(&cat{1, "Pupuru"})
 	q.Enqueue(&cat{3, "Hosico"})
 
-	if q.Print() != "[&{1 Pupuru} &{3 Hosico}]" {
-		t.Errorf("Expected: [&{1 Pupuru} &{3 Hosico}], Got: %v", q.Print())
-	}
+	assert.Equal(t, "[&{1 Pupuru} &{3 Hosico}]", q.Print())
 }
 
 func TestDequeue(t *testing.T) {
@@ -38,26 +34,16 @@ func TestDequeue(t *testing.T) {
 	q.Enqueue(&cat{1, "Ah Fei"})
 	v := q.Dequeue().(*cat)
 
-	if cmp.Equal(v, &cat{5, "Yuen Jumbo"}) == false {
-		t.Errorf("Expected: &{5 Yuen Jumbo}, Got: %v", v)
-	}
-
-	if q.Print() != "[&{3 Fomo} &{1 Ah Fei}]" {
-		t.Errorf("Expected: [&{3 Fomo} &{1 Ah Fei}], Got: %v", q.Print())
-	}
+	assert.Equal(t, v, &cat{5, "Yuen Jumbo"})
+	assert.Equal(t, q.Print(), "[&{3 Fomo} &{1 Ah Fei}]")
 }
 
 func TestDequeueOnEmptyQueue(t *testing.T) {
 	q := queue.NewQueue()
 	v := q.Dequeue()
 
-	if v != nil {
-		t.Errorf("Expected: nil, Got: %v", q.Print())
-	}
-
-	if q.Print() != "[]" {
-		t.Errorf("Expected: [], Got: %v", q.Print())
-	}
+	assert.Nil(t, v)
+	assert.Equal(t, "[]", q.Print())
 }
 
 func TestPeek(t *testing.T) {
@@ -66,26 +52,16 @@ func TestPeek(t *testing.T) {
 	q.Enqueue(&cat{2, "Bombi"})
 	v := q.Peek().(*cat)
 
-	if cmp.Equal(v, &cat{1, "Lupita"}) == false {
-		t.Errorf("Expected: &cat{1 Lupita}, Got: %v", q.Print())
-	}
-
-	if q.Print() != "[&{1 Lupita} &{2 Bombi}]" {
-		t.Errorf("Expected: [&{1 Lupita} &{2 Bombi}], Got: %v", q.Print())
-	}
+	assert.Equal(t, v, &cat{1, "Lupita"})
+	assert.Equal(t, "[&{1 Lupita} &{2 Bombi}]", q.Print())
 }
 
 func TestPeekOnEmptyQueue(t *testing.T) {
 	q := queue.NewQueue()
 	v := q.Peek()
 
-	if v != nil {
-		t.Errorf("Expected: nil, Got: %v", q.Print())
-	}
-
-	if q.Print() != "[]" {
-		t.Errorf("Expected: [], Got: %v", q.Print())
-	}
+	assert.Nil(t, v)
+	assert.Equal(t, "[]", q.Print())
 }
 
 func TestClear(t *testing.T) {
@@ -94,24 +70,18 @@ func TestClear(t *testing.T) {
 	q.Enqueue(&cat{20, "Big Billy"})
 	q.Clear()
 
-	if q.Print() != "[]" {
-		t.Errorf("Expected: [], Got: %v", q.Print())
-	}
+	assert.Equal(t, "[]", q.Print())
 }
 
 func TestIsEmptyOnEmptyQueue(t *testing.T) {
 	q := queue.NewQueue()
 
-	if q.IsEmpty() == false {
-		t.Errorf("Expected: true, Got: %v", q.Print())
-	}
+	assert.True(t, q.IsEmpty())
 }
 
 func TestIsEmptyOnNotEmptyQueue(t *testing.T) {
 	q := queue.NewQueue()
 	q.Enqueue(&cat{7, "Bellamy"})
 
-	if q.IsEmpty() == true {
-		t.Errorf("Expected: false, Got: %v", q.Print())
-	}
+	assert.False(t, q.IsEmpty())
 }
