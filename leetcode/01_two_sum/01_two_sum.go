9244303ad0a7ed8673420c9179ac30eeac598bbc
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2021-05-01 19:06:01 +0700

    Update Two Sum test case

--- leetcode/01_two_sum/01_two_sum.go
+++ leetcode/01_two_sum/01_two_sum.go
@@ -6,7 +6,7 @@ import "fmt"
 // https://leetcode.com/problems/two-sum/
 func twoSum(nums []int, target int) []int {
 	for i := 0; i < len(nums)-1; i++ {
-		for j := 1; j < len(nums); j++ {
+		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] == target {
 				return []int{i, j}
 			}
@@ -20,4 +20,5 @@ func main() {
 	fmt.Printf("Result: %v, Expected: %v\n", twoSum([]int{3, 2, 4}, 6), []int{1, 2})
 	fmt.Printf("Result: %v, Expected: %v\n", twoSum([]int{3, 3}, 6), []int{0, 1})
 	fmt.Printf("Result: %v, Expected: %v\n", twoSum([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 19), []int{8, 9})
+	fmt.Printf("Result: %v, Expected: %v\n", twoSum([]int{2, 5, 5, 10}, 10), []int{1, 2})
 }
