6c2e5724f7a97a7c1fc831cf7090e0a33d403673
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2021-08-07 08:11:12 +0700

    Add more test cases for LC String to Integer (atoi)

--- leetcode/08_string_to_integer_atoi/08_string_to_integer_atoi_test.go
+++ leetcode/08_string_to_integer_atoi/08_string_to_integer_atoi_test.go
@@ -7,7 +7,34 @@ import (
 )
 
 func TestMyAtoi(t *testing.T) {
+	a.Equal(t, 0, myAtoi(""))
+	a.Equal(t, 0, myAtoi(" "))
+	a.Equal(t, 0, myAtoi("  "))
+
+	a.Equal(t, 0, myAtoi("   -"))
+	a.Equal(t, 0, myAtoi("   - abc"))
+	a.Equal(t, 0, myAtoi("   --"))
+	a.Equal(t, 0, myAtoi("   -+12"))
+	a.Equal(t, 0, myAtoi("        +"))
+	a.Equal(t, 0, myAtoi("        +abc"))
+
 	a.Equal(t, 42, myAtoi("42"))
+	a.Equal(t, 42, myAtoi("+42"))
 	a.Equal(t, -42, myAtoi("   -42"))
+	a.Equal(t, 42, myAtoi("   +42"))
 	a.Equal(t, 4193, myAtoi("4193 with words"))
+
+	a.Equal(t, 2147483647, myAtoi("2147483648"))
+	a.Equal(t, 2147483647, myAtoi("9999999999"))
+	a.Equal(t, -2147483648, myAtoi("-2147483649"))
+	a.Equal(t, -2147483648, myAtoi("-9999999999"))
+	a.Equal(t, 2147483647, myAtoi(" 2147483648lol"))
+	a.Equal(t, -2147483648, myAtoi(" -2147483649lol"))
+
+	a.Equal(t, 2147483647, myAtoi("20000000000000000000"))
+	a.Equal(t, -2147483648, myAtoi("-20000000000000000000"))
+
+	a.Equal(t, 12345678, myAtoi("  0000000000012345678"))
+	a.Equal(t, 12345678, myAtoi("  +0000000000012345678"))
+	a.Equal(t, -12345678, myAtoi("  -0000000000012345678"))
 }
