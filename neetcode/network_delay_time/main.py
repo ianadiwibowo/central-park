cdd67ec18d9e3e33d5400874f86e18201fb98186
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2025-04-16 09:48:23 +0700

    feat: Tidy-up explanation comments in NeetCode: Network Delay Time

--- neetcode/network_delay_time/main.py
+++ neetcode/network_delay_time/main.py
@@ -4,14 +4,17 @@ from typing import List
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # Construct graph (adjacency list)
         graphs = defaultdict(list)
         for origin_node, destination_node, delay_time in times:
             graphs[origin_node].append((destination_node, delay_time))
 
+        # Construct initial distances from k to all nodes
         distances = {}
         for node in range(1, n+1):
             distances[node] = float("inf")
 
+        # Propagate
         def propagate(node, time):
             if time >= distances[node]:
                 return
@@ -19,7 +22,10 @@ class Solution:
             for neighbor_node, weight in graphs[node]:
                 propagate(neighbor_node, time + weight)
 
+        # Propagate from node k with initial 0 time
         propagate(k, 0)
+
+        # Return max distances
         shortest_time_to_visit_all = max(distances.values())
         if shortest_time_to_visit_all == float("inf"):
             return -1
